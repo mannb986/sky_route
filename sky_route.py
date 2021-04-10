0be40acfb3513c11edcdfd9dcbf038d3cf5efaca
Brian Mann <mannb986@gmail.com>
2021-04-10 16:08:08 +0200

    further functions added

--- sky_route.py
+++ sky_route.py
@@ -15,9 +15,11 @@ def greet():
 
 def skyroute():
     greet()
+    new_route()
+    goodbye()
 
 def set_start_and_end(start_point, end_point):
-    if start_point:
+    if start_point is not None:
         change_point = input("What would you like to change? You can enter 'o' for 'origin', 'd' for 'destination', or 'b' for 'both: ")
         if change_point == "b":
             start_point = get_start()
@@ -38,6 +40,7 @@ def get_start():
     start_point_letter = input("Where are you coming from? Type in the correspondig letter: ")
     if start_point_letter in landmark_choices:
         start_point = landmark_choices[start_point_letter]
+        return start_point
     else:
         print("Sorry, that's not a landmark we have data on. Let's try this again...")
         get_start()
@@ -46,12 +49,28 @@ def get_end():
     end_point_letter = input("Ok, where are you headed? Type in the corresponding letter: ")
     if end_point_letter in landmark_choices:
         end_point = landmark_choices[end_point_letter]
+        return end_point
     else:
         print("Sorry, that's not a landmark we have data on. Let's try this again...")
         get_end() 
 
 def new_route(start_point=None, end_point=None):
     start_point, end_point = set_start_and_end(start_point, end_point)
+    shortest_route = get_route(start_point, end_point)
+    shortest_route_string = '\n'.join(shortest_route)
+    print("The shortest metro route from {0} to {1} is:\n{2}".format(start_point, end_point, shortest_route_string))
+    again = input("Would you like to see another route? Enter y/n: ")
+    if again == "y":
+        show_landmarks()
+        new_route(start_point, end_point)
+
+def show_landmarks():
+    see_landmarks = input("Would you like to see the list of landmarks again? Enter y/n: ")
+    if see_landmarks == "y":
+        print(landmark_string)
+
+def goodbye():
+    print("Thanks for using SkyRoute!")
 
 def get_route(start_point, end_point):
     start_stations = vc_landmarks[start_point]
@@ -68,7 +87,7 @@ def get_route(start_point, end_point):
 
 
 ####### Testing Area ########
-# skyroute()
+skyroute()
 # print(set_start_and_end(None, None))
 # set_start_and_end(None, None)
 # print(get_route('Marine Building', 'Canada Place'))
