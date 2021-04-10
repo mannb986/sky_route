1183f25f989565b6dfc27e16ecb0684f69e18276
Brian Mann <mannb986@gmail.com>
2021-04-10 13:55:44 +0200

    greet function created and skyroute function defined

--- sky_route.py
+++ sky_route.py
@@ -4,3 +4,15 @@ from vc_landmarks import vc_landmarks
 from landmark_choices import landmark_choices
 
 # Build your program below:
+landmark_string = ""
+for letter, landmark in landmark_choices.items():
+    landmark_string += "{0} - {1}\n".format(letter, landmark)
+
+def greet():
+    print("Hi there and welcome to SkyRoute!")
+    print("We'll help you find the shortest route between the following Vancouver landmarks:\n{0}".format(landmark_string))
+
+def skyroute():
+    greet()
+
+skyroute()
